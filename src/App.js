4be4b074846b1e802fceddb95fd7160e7d17e7c8
IoanaCiuca <ioanamciuca@yahoo.com>
2024-03-21 06:54:19 -0400

    list of entries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+//fucntional component has been created using ES6 style
+import axios from 'axios';
+import React, {useState, useEffect} from "react";
 
-function App() {
+const App = (props)=> {
+    //define the state and the setter
+    const [APIlist, setAPIlist] = useState();
+
+    //On load invoke method to genrate list
+    useEffect(() => {  let url = "https://api.publicapis.org/entries?category=Animals";
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'json'
+    }).then(resp => {
+        let listOfEntries = resp.data.entries;
+        let listOfEntriesAsArray = Object.entries(listOfEntries);
+        let i=1;
+        let entryDetails = listOfEntriesAsArray.map((entryDetail)=>{
+          return(
+          <li key={i++}>
+            <a href={entryDetail[1]["Link"]} target="_blank" rel="noreferrer">{entryDetail[1]["API"]}</a>
+            </li>
+          )
+        })
+        setAPIlist(entryDetails);
+      })
+    .catch(err => {
+        console.log(err.toString())
+    });
+    }, []);
+
+
+    const colorStyle = { color:props['color'], fontSize:props['size']}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>APIs List</h2>
+        <br/>
+
+            <div style={colorStyle}><ul>{APIlist}</ul></div>
     </div>
-  );
+);
 }
 
 export default App;
